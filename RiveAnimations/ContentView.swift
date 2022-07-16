bc26a380c7a601367f4a5c59ed409cf86f8d2d64
Jonathan Go <gojonathan.dev@gmail.com>
2022-07-16 14:16:32 +0200

    Initial Commit

---
+++ RiveAnimations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RiveAnimations
+//
+//  Created by Jonathan Go on 16.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RiveAnimations/RiveAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveAnimationsApp.swift
+//  RiveAnimations
+//
+//  Created by Jonathan Go on 16.07.22.
+//
+
+import SwiftUI
+
+@main
+struct RiveAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
