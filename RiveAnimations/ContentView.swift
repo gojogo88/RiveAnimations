e9b92c54d906c5f120dc2ee790266c7957c1ed50
Jonathan Go <gojonathan.dev@gmail.com>
2022-07-17 12:33:36 +0200

    Create menu button

--- RiveAnimations/ContentView.swift
+++ RiveAnimations/ContentView.swift
@@ -4,11 +4,13 @@
 //
 //  Created by Jonathan Go on 16.07.22.
 //
-
+import RiveRuntime
 import SwiftUI
 
 struct ContentView: View {
     @AppStorage("selectedTab") var selectedTab: Tab = .chat
+    @State private var isOpen = false
+    let menuButton = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
     
     var body: some View {
         ZStack {
@@ -24,7 +26,17 @@ struct ContentView: View {
             case .user:
                 Text("User")
             }
-
+            
+            menuButton.view()
+                .frame(width: 44, height: 44)
+                .mask(Circle())
+                .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x: 0, y: 5)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding()
+                .onTapGesture {
+                    menuButton.setInput("isOpen", value: isOpen)
+                    isOpen.toggle()
+                }
             TabBar()
         }
     }
