88e934e891d4d3f1dc987e15bdfba5152c91ab23
Jonathan Go <gojonathan.dev@gmail.com>
2022-07-17 11:05:41 +0200

    Handle TabItem selection state and presentation

--- RiveAnimations/ContentView.swift
+++ RiveAnimations/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("selectedTab") var selectedTab: Tab = .chat
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            switch selectedTab {
+            case .chat:
+                Text("Chat")
+            case .search:
+                Text("Search")
+            case .timer:
+                Text("Timer")
+            case .bell:
+                Text("Bell")
+            case .user:
+                Text("User")
+            }
+
+            TabBar()
+        }
     }
 }
 
